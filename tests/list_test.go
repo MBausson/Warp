d33c05e71fd287b3b7ecaef9fe2fe7d876fcc0b8
MBausson <maximilienbausson@icloud.com>
2025-03-06 21:54:26 +0100

    added base test

---
+++ tests/list_test.go
@@ -0,0 +1,20 @@
+package warp_test
+
+import (
+	"os/exec"
+	"testing"
+)
+
+func execute(command string) (string, error) {
+	const powershellPath = `C:\Program Files\PowerShell\7\pwsh.exe`
+
+	cmd := exec.Command(powershellPath, "-Command", command)
+
+	output, err := cmd.Output()
+
+	return string(output), err
+}
+
+func TestList(t *testing.T) {
+	execute("./warp.ps1 list")
+}
