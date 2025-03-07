22b3d5a2a9578b57a2b8cd2731abedbc445d458a
MBausson <maximilienbausson@icloud.com>
2025-03-07 16:58:50 +0100

    wip

---
+++ tests/powershell_path_unix.go
@@ -0,0 +1,5 @@
+//go:build !windows
+
+package tests
+
+const powerhsellPath = `/bin/pwsh`
--- tests/utils.go
+++ tests/utils.go
@@ -3,9 +3,7 @@ package tests
 import "os/exec"
 
 func execute(command string) (string, error) {
-	const powershellPath = `C:\Program Files\PowerShell\7\pwsh.exe`
-
-	cmd := exec.Command(powershellPath, "-Command", command)
+	cmd := exec.Command(powerhsellPath, "-Command", command)
 
 	output, err := cmd.Output()
 
